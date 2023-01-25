1f5dd403a6ffd754985f68a76193a9dc1910216d
Jordan Reed <reed5204@vandals.uidaho.edu>
2023-01-25 14:19:52 -0800

    put terminal commands into shell script for easier executing

--- assign1/commands.sh
+++ assign1/commands.sh
@@ -1,13 +1,13 @@
+#!/bin/bash
+
 ros2 action send_goal /create3_0620/undock irobot_create_msgs/action/Undock {}
 
 ros2 action send_goal /create3_0620/drive_distance irobot_create_msgs/action/DriveDistance "{distance: 1.0}"
 
-45 degrees
 ros2 action send_goal /create3_0620/rotate_angle irobot_create_msgs/action/RotateAngle "{angle: .785}"
 
 ros2 action send_goal /create3_0620/drive_distance irobot_create_msgs/action/DriveDistance "{distance: 0.5}"
 
-135 degrees
 ros2 action send_goal /create3_0620/rotate_angle irobot_create_msgs/action/RotateAngle "{angle: 2.36}"
 
 ros2 action send_goal /create3_0620/drive_distance irobot_create_msgs/action/DriveDistance "{distance: 0.6}"
