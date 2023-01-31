dcbe870892b059c164a93964f0dc52f5fc1673aa
Jordan Reed <reed5204@vandals.uidaho.edu>
2023-01-30 23:33:17 -0800

    Fixed code to have callbacks and spin node until action is done

--- assign1/workspace/src/robotWalk/robotWalk/main.py
+++ assign1/workspace/src/robotWalk/robotWalk/main.py
@@ -9,6 +9,8 @@
 #           robot will move forward 1m, turn 45 degree, move 
 #           forward .5m, and return home.
 #
+#       Much help from James
+#
 # ------------------------------------------
 
 import rclpy
@@ -18,118 +20,109 @@ from rclpy.action.client import ActionClient
 import irobot_create_msgs
 from irobot_create_msgs.action import DriveDistance, Undock, Dock, RotateAngle
 
+from action_msgs.msg import GoalStatus
+
 class MyNode(Node):
+    """ A class that will create a single node, and reuse a single action client, to move a robot.
+    """
     def __init__(self, namespace:str):
         # call superclass init
         super().__init__('walker')
 
-        print(f'constructing {namespace} node')
-        self.namespace = namespace
+        print(f"Constructing '{namespace}' node.")
+        self._namespace = namespace
+        self.result = None
 
-        # need action clients for each command
-        self.drive_ac = ActionClient(self, DriveDistance, f'/{self.namespace}/drive_distance')
-        self.undock_ac = ActionClient(self, Undock, f'/{self.namespace}/undock')
-        self.dock_ac = ActionClient(self, Dock, f'/{self.namespace}/dock')
-        self.rotate_ac = ActionClient(self, RotateAngle, f'/{self.namespace}/rotate_angle')
+        self._action_client = None
     
-    def undock_robot(self):
-        print("now executing undock command")
+    def send_goal(self, action_type, action_name:str, goal):
+        self.get_logger().info(f"Sending goal for '{action_name}'")
+        # create/reuse action client with new goal info
+        self._action_client = ActionClient(self, action_type, f'/{self._namespace}/{action_name}')
 
         # wait for server
-        self.get_logger().warning("waiting for server...")
-        self.undock_ac.wait_for_server()
+        self.get_logger().warning("Waiting for server...")
+        self._action_client.wait_for_server()
 
         # server available
-        self.get_logger().warning("server available!")
-        self.get_logger().warning("sending goal now...")
-
-        # create goal object
-        undock_goal = Undock.Goal()
+        self.get_logger().warning("Server available. Sending goal now...")
 
         # send goal
-        self.undock_ac.send_goal_async(undock_goal)
-        # self.get_logger().warning("robot is UNDOCKED")
+        self.send_goal_future = self._action_client.send_goal_async(goal) #, self.feedback_callback)
+
+        # add done callback for response
+        self.send_goal_future.add_done_callback(self.goal_response_callback)
+
+        # spin node until done
+        self.result = None
+        while self.result == None:
+            rclpy.spin_once(self)
+
+        # goal done
+        self.get_logger().warning(f"{action_name} action done")
     
-    def dock_robot(self):
-        print("now executing dock command")
-
-        # wait for server
-        self.get_logger().warning("waiting for server...")
-        self.dock_ac.wait_for_server()
-
-        # server available
-        self.get_logger().warning("server available!")
-        self.get_logger().warning("sending goal now...")
-
-        # create goal object
-        dock_goal = Dock.Goal()
-
-        # send goal
-        self.dock_ac.send_goal_async(dock_goal)
-        # self.get_logger().warning("robot is DOCKED")
+    def feedback_callback(self, feedback):
+        self.get_logger().info(f'received feedback: {feedback}')
     
-    def drive_robot(self, drive_dist:float):
-        print(f'starting drive command {drive_dist}m')
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        # print(f'goal handle: {goal_handle}')
+        if not goal_handle.accepted:
+            self.get_logger().error("GOAL REJECTED")
+            return
 
-        # wait for server
-        self.get_logger().warning("waiting for server")
-        self.drive_ac.wait_for_server()
-
-        # server avail
-        self.get_logger().warning("server available. now sending goal")
-
-        # create goal object
-        drive_goal = DriveDistance.Goal()
-        drive_goal.distance = drive_dist
-
-        # send goal
-        self.drive_ac.send_goal_async(drive_goal)
-
-        # create goal finished message?
-        self.get_logger().info("goal sent and executing")
+        self.get_result_future = goal_handle.get_result_async()
+        self.get_result_future.add_done_callback(self.get_result_callback)
     
-    def rotate_robot(self, angle:float):
+    def get_result_callback(self, future):
+        self.result = future.result().result
+        status = future.result().status
+
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info("Goal Succeeded! Result info hidden.")
+        else:
+            self.get_logger().error(f"Goal Failed with status: {status}")
         
-        # wait for server
-        self.get_logger().info("waiting for server")
-        self.rotate_ac.wait_for_server()
-
-        # server available
-        self.get_logger().info("Server AVAILABLE!")
-
-        # create goal object
-        rotate_goal = RotateAngle.Goal()
-        rotate_goal.angle = angle
-
-        # send goal (async because wayland issue??)
-        self.get_logger().info("sending goal now!")
-        self.rotate_ac.send_goal(rotate_goal)
-
-        # send complete message
-        self.get_logger().info("goal sent!")
-        
-    
-    def command(self):
-        ''' robot will drive forward 1m, turn 45deg, drive .5m, turn 135deg, drive .6m'''
-        self.undock_robot()
-        self.drive_robot(1.0)
-        self.rotate_robot(3.14159/4)
-        self.drive_robot(.5)
-        self.rotate_robot(3.14159*3/4)
-        self.drive_robot(.6)
-        self.dock_robot()
-
 def main():
-    print("Hello there! Just testing out the first robot program")
-
     rclpy.init()
     node = MyNode("create3_0620")
 
-    node.command()
+    # undock
+    cur_goal = Undock.Goal()
+    node.send_goal(Undock, 'undock', cur_goal)
 
-    rclpy.spin(node)
+    # drive for 1m
+    cur_goal = DriveDistance.Goal()
+    cur_goal.distance = 1.0
+    node.send_goal(DriveDistance, 'drive_distance', cur_goal)
+
+    # turn 45deg
+    cur_goal = RotateAngle.Goal()
+    cur_goal.angle = 3.14159/4
+    node.send_goal(RotateAngle, 'rotate_angle', cur_goal)
+
+    # drive 0.5m
+    cur_goal = DriveDistance.Goal()
+    cur_goal.distance = 0.5
+    node.send_goal(DriveDistance, 'drive_distance', cur_goal)
+
+    # rotate 130deg
+    cur_goal = RotateAngle.Goal()
+    cur_goal.angle = 3.14159*3/4
+    node.send_goal(RotateAngle, 'rotate_angle', cur_goal)
+
+    # drive 0.6m
+    cur_goal = DriveDistance.Goal()
+    cur_goal.distance = 0.6
+    node.send_goal(DriveDistance, 'drive_distance', cur_goal)
+
+    # dock robot
+    cur_goal = Dock.Goal()
+    node.send_goal(Dock, 'dock', cur_goal)
+
+    # shut down node
     rclpy.shutdown()
 
 if __name__ == '__main__':
-    print('code is run as python script, not package')
+    print('--PYTHON SCRIPT--')
     main()
